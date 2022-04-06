4c8e203f63132a3a92a5894abf3646d14ec1e2d0
Devin.Zhu <chooin@126.com>
2022-04-06 17:33:59 +0800

    chore: umi3

--- packages/mrm-task-umi3/templates/src/utils/parse-query.ts
+++ packages/mrm-task-umi3/templates/src/utils/parse-query.ts
@@ -1,30 +1,12 @@
 import type { StringifyOptions, ParseOptions } from 'query-string';
 import queryString from 'query-string';
 
-type Options = StringifyOptions &
-  ParseOptions & {
-    trimStrings?: boolean;
-  };
+type Options = StringifyOptions & ParseOptions;
 
-type Query = Record<string, string | number | (string | number)[] | null>;
+type Query = Record<string, string | number[] | null>;
 
 export function parseQuery<T extends {}>(query: Query, options: Options): T;
 
 export function parseQuery(query: Query, options: Options) {
-  if (options.trimStrings) {
-    Object.entries(query).forEach(([key, value]) => {
-      if (typeof value === 'string') {
-        query[key] = value.trim();
-      } else if (Array.isArray(value)) {
-        query[key] = value.map((v) => {
-          if (typeof v === 'string') {
-            return v.trim();
-          }
-          return v;
-        });
-      }
-    });
-  }
-
   return queryString.parse(queryString.stringify(query, options), options);
 }
