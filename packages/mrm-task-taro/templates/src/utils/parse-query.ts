db6ddb210e6c0e1a7743e911e3e15843568b48e6
Devin.Zhu <chooin@126.com>
2022-04-20 11:36:54 +0800

    chore: parse query

--- packages/mrm-task-taro/templates/src/utils/parse-query.ts
+++ packages/mrm-task-taro/templates/src/utils/parse-query.ts
@@ -1,14 +1,12 @@
-import queryString, { StringifyOptions, ParseOptions } from 'query-string';
+import type { StringifyOptions, ParseOptions } from 'query-string';
+import queryString from 'query-string';
 
-type Options = StringifyOptions &
-  ParseOptions & {
-    trimStrings?: boolean;
-  };
+type Options = StringifyOptions & ParseOptions;
 
 type Query = Partial<Record<string, string>>;
 
-export function parseQuery<T extends {}>(query: Query, options: Options): T;
+export function parseQuery<T extends {}>(query: Query, options?: Options): T;
 
-export function parseQuery(query: Query, options: Options) {
+export function parseQuery(query: Query, options: Options = {}) {
   return queryString.parse(queryString.stringify(query, options), options);
 }
--- packages/mrm-task-umi3/templates/src/utils/parse-query.ts
+++ packages/mrm-task-umi3/templates/src/utils/parse-query.ts
@@ -5,8 +5,8 @@ type Options = StringifyOptions & ParseOptions;
 
 type Query = Record<string, string | string[] | null>;
 
-export function parseQuery<T extends {}>(query: Query, options: Options): T;
+export function parseQuery<T extends {}>(query: Query, options?: Options): T;
 
-export function parseQuery(query: Query, options: Options) {
+export function parseQuery(query: Query, options: Options = {}) {
   return queryString.parse(queryString.stringify(query, options), options);
 }
