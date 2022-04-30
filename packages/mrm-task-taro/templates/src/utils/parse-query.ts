4ac3f4336fbc0613cc61bf54633537bc80915a7c
Devin.Zhu <chooin@126.com>
2022-05-01 03:03:19 +0800

    chore: umi3

--- packages/mrm-task-taro/templates/src/utils/parse-query.ts
+++ packages/mrm-task-taro/templates/src/utils/parse-query.ts
@@ -5,7 +5,7 @@ type Options = StringifyOptions & ParseOptions;
 
 type Query = Partial<Record<string, string>>;
 
-export function parseQuery<T extends {}>(query: Query, options?: Options): T;
+export function parseQuery<T = {}>(query: Query, options?: Options): T;
 
 export function parseQuery(query: Query, options: Options = {}) {
   return queryString.parse(queryString.stringify(query, options), options);
--- packages/mrm-task-umi3/templates/src/utils/parse-query.ts
+++ packages/mrm-task-umi3/templates/src/utils/parse-query.ts
@@ -5,7 +5,7 @@ type Options = StringifyOptions & ParseOptions;
 
 type Query = Record<string, string | string[] | null>;
 
-export function parseQuery<T extends {}>(query: Query, options?: Options): T;
+export function parseQuery<T = {}>(query: Query, options?: Options): T;
 
 export function parseQuery(query: Query, options: Options = {}) {
   return queryString.parse(queryString.stringify(query, options), options);
