9968a3e000c6671d24bcdb7f268eddbe235d8bf5
Devin.Zhu <chooin@126.com>
2022-06-09 00:48:08 +0800

    chore: laravel

--- packages/mrm-task-laravel/index.ts
+++ packages/mrm-task-laravel/index.ts
@@ -2,7 +2,12 @@ import { template } from 'mrm-core';
 import path from 'path';
 
 function addFiles() {
-  const files = ['docker-compose.yml', 'Dockerfile', 'laravel.ini'];
+  const files = [
+    'docker-compose.production.yml',
+    'docker-compose.testing.yml',
+    'Dockerfile',
+    'laravel.ini',
+  ];
 
   files.forEach((file) => {
     template(file, path.join(__dirname, 'templates', file)).apply().save();
