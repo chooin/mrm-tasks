8c6b46fe164ab8c29e754fc833c00abd1d7fb7eb
Devin.Zhu <chooin@126.com>
2022-07-06 15:47:37 +0800

    feat: yarnrc

--- packages/mrm-task-umi3/templates/src/app.tsx
+++ packages/mrm-task-umi3/templates/src/app.tsx
@@ -3,6 +3,6 @@ import NiceModal from '@ebay/nice-modal-react';
 
 export async function getInitialState() {}
 
-export function rootContainer(container: React.FC) {
+export function rootContainer(container: React.ReactElement) {
   return React.createElement(NiceModal.Provider, null, container);
 }
